a51d274677e97070bd9170d501f864f3b26fd32c
Pemba Sherpa <127815083+PembaSrpa@users.noreply.github.com>
2026-06-04 12:41:13 +0545

    remake 14

--- components/Scales.tsx
+++ components/Scales.tsx
@@ -2,14 +2,14 @@ export function Scales() {
   return (
     <>
       <div
-        className="pointer-events-none absolute left-0 top-0 h-full w-5 border-r border-neutral-300 dark:border-neutral-700 z-[10]"
+        className="pointer-events-none absolute left-0 top-0 h-full w-5 border-r border-neutral-400 dark:border-neutral-600 z-[10]"
         style={{
           backgroundImage: 'repeating-linear-gradient(315deg,var(--pattern-fg) 0,var(--pattern-fg) 1px,transparent 0,transparent 50%)',
           backgroundSize: '8px 8px',
         }}
       />
       <div
-        className="pointer-events-none absolute right-0 top-0 h-full w-5 border-l border-neutral-300 dark:border-neutral-700 z-[10]"
+        className="pointer-events-none absolute right-0 top-0 h-full w-5 border-l border-neutral-400 dark:border-neutral-600 z-[10]"
         style={{
           backgroundImage: 'repeating-linear-gradient(315deg,var(--pattern-fg) 0,var(--pattern-fg) 1px,transparent 0,transparent 50%)',
           backgroundSize: '8px 8px',
